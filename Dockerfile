ca4fb26a53fcd148e3ea7719e4ef7598184abf2c
Pierre <pierre.taiton@outlook.fr>
2020-02-21 09:07:42 +0100

    update

--- Dockerfile
+++ Dockerfile
@@ -10,3 +10,6 @@ ENV MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
 ENV MYSQL_DATABASE=recipe
 
 COPY ./migrations/ /docker-entrypoint-initdb.d/
+COPY ./conf/conf.d /etc/mysql/conf.d
+
+CMD ["--default-authentication-plugin=mysql_native_password"]
--- migrations/V202002191535_init-schema.sql
+++ migrations/V202002191535_init-schema.sql
@@ -1,53 +1,33 @@
-create schema if not exists recipe collate latin1_swedish_ci;
-
-create table if not exists Ingredient
-(
-	Id varchar(255) charset utf8mb4 not null
-		primary key,
-	Name longtext charset utf8mb4 null
+create table if not exists ingredient (
+	id varchar(255) not null primary key,
+	name longtext null
 );
 
-create table if not exists Recipe
-(
-	Id varchar(255) charset utf8mb4 not null
-		primary key,
-	Name longtext charset utf8mb4 null,
-	NumberOfPersons int null,
-	ImageBase64 longtext charset utf8mb4 null
+create table if not exists recipe (
+	id varchar(255) not null primary key,
+	name longtext null,
+	number_of_persons int null,
+	image longtext null
 );
 
-create table if not exists IngredientRecipe
-(
-	IngredientId varchar(255) charset utf8mb4 not null,
-	RecipeId varchar(255) charset utf8mb4 not null,
-	Unit longtext charset utf8mb4 null,
-	Quantity int not null,
-	primary key (IngredientId, RecipeId),
-	constraint FK_IngredientRecipe_Ingredient_IngredientId
-		foreign key (IngredientId) references Ingredient (Id)
-			on delete cascade,
-	constraint FK_IngredientRecipe_Recipe_IngredientId
-		foreign key (RecipeId) references Recipe (Id)
-			on delete cascade
+create table if not exists ingredient_recipe (
+	id_ingredient varchar(255) not null,
+	id_recipe varchar(255) not null,
+	unit longtext null,
+	quantity int not null,
+	
+	primary key (id_ingredient, id_recipe),
+	foreign key (id_ingredient) references ingredient (id) on delete cascade,
+	foreign key (id_recipe) references recipe (id) on delete cascade
 );
 
-create table if not exists Step
-(
-	Id varchar(255) charset utf8mb4 not null
-		primary key,
-	Number int not null,
-	Description longtext charset utf8mb4 null,
-	RecipeId varchar(255) charset utf8mb4 null,
-	constraint FK_Step_Recipe_RecipeId
-		foreign key (RecipeId) references Recipe (Id)
+create table if not exists step (
+	id varchar(255) not null primary key,
+	number int not null,
+	description longtext null,
+	id_recipe varchar(255) null,
+	
+	foreign key (id_recipe) references recipe (id)
 );
 
-create index IX_Step_RecipeId
-	on Step (RecipeId);
-
-create table if not exists __EFMigrationsHistory
-(
-	MigrationId varchar(95) not null
-		primary key,
-	ProductVersion varchar(32) not null
-);
+create index IX_Step_id_recipe on step (id_recipe);
